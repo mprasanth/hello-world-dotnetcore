27243b02ac9dccd287f5a79f84450d109ab0f824
Prasanth Maddali <maddali.prasanth@gmail.com>
2021-07-16 12:15:49 +0800

    Added secrets manager

---
+++ Controllers/SecretsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace hello_world_core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretsController : ControllerBase
+    {
+        
+        [HttpGet]
+        public string Get()
+        {
+            var secret = (new SecretManager()).Get("FirstSecret");
+            return secret;
+        }
+    }
+}
---
+++ SecretsManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+namespace hello_world_core
+{
+    public class SecretManager
+    {
+        public string Get(string secretName)
+        {
+            var config = new AmazonSecretsManagerConfig { RegionEndpoint = RegionEndpoint.APSoutheast1 };
+            var client = new AmazonSecretsManagerClient(config);
+
+            var request = new GetSecretValueRequest
+            {
+                SecretId = secretName
+            };
+
+            GetSecretValueResponse response = null;
+            try
+            {
+                response = Task.Run(async () => await client.GetSecretValueAsync(request)).Result;
+            }
+            catch (ResourceNotFoundException)
+            {
+                Console.WriteLine("The requested secret " + secretName + " was not found");
+            }
+            catch (InvalidRequestException e)
+            {
+                Console.WriteLine("The request was invalid due to: " + e.Message);
+            }
+            catch (InvalidParameterException e)
+            {
+                Console.WriteLine("The request had invalid params: " + e.Message);
+            }
+
+            return response?.SecretString;
+        }
+    }
+}
